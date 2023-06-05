237f9a2c9304dde43691d5fbd893b47bd184d14a
aakhare <aastha.khare@gmail.com>
2023-06-04 23:54:40 -0700

    adjusted or gate for 3 inputs

--- orGate.v
+++ orGate.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 
-module or_gate(d1, d2, out);
-input d1 ,d2;
+module or_gate(d1, d2, d3, out);
+input d1 ,d2, d3;
 output reg out;
 
-always@(d1, d2)
+    always@(d1, d2, d3)
 begin
-    out = d1 | d2;
+    out = d1 | d2 | d3;
 end
 
 endmodule
